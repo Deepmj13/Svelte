9c14fed4ddd92721685e56157f2dfbc3250a551b
Jerry <playstore1392004@gmail.com>
2025-12-28 13:07:36 +0530

    Boiler

---
+++ rockpaperscissor/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  let massage = "Hello world";
+</script>
+
+<div class="container">
+  <div class="content">
+    <h1>Rock Papper Scissor</h1>
+    <div class="game">
+      <h3>Choose your move</h3>
+      <div class="moves">
+        <span class="move"> 👊 </span>
+        <span class="move"> 🖐️ </span>
+        <span class="move"> ✌️ </span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .container {
+    width: 100vw;
+    height: 100vh;
+    background-color: #000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .content {
+    text-align: center;
+    color: #fff;
+  }
+</style>
